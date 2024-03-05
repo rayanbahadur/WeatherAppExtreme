be652a9fdc63d65ee1444842d3b0e5fcceeae6c8
Rayan Bahadur <rayanbahadur16@gmail.com>
2024-03-05 17:38:58 +0000

    Remove unnecessary files and code

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-2xl font-bold text-red-500'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
